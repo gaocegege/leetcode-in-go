ec575bb974bfcd9f9bae9be8d11d17b4c37d4711
Ce Gao <ce.gao@outlook.com>
2017-10-08 12:27:21 +0800

    Fuck you Leetcode
    Signed-off-by: Ce Gao <ce.gao@outlook.com>

---
+++ Medium/Most-Frequent-Subtree-Sum/README.md
@@ -0,0 +1,3 @@
+# Bug
+
+Output in `Run Code` is [1], but output in `Submit` is [2]
---
+++ Medium/Most-Frequent-Subtree-Sum/main.go
@@ -0,0 +1,34 @@
+package main
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+var resMap = make(map[int]int)
+
+var maxTime = 0
+
+func findFrequentTreeSum(root *TreeNode) []int {
+	res := make([]int, 0)
+	getAndSetSum(root)
+	for k, v := range resMap {
+		if v == maxTime {
+			res = append(res, k)
+		}
+	}
+	return res
+}
+
+func getAndSetSum(tn *TreeNode) int {
+	if tn == nil {
+		return 0
+	}
+	score := tn.Val + getAndSetSum(tn.Left) + getAndSetSum(tn.Right)
+	resMap[score] = resMap[score] + 1
+	if resMap[score] > maxTime {
+		maxTime = resMap[score]
+	}
+	return score
+}
